b22bca27330c3363eb1d5ac043bb392d2b3313f1
Roldan <jesus.roldan82@hotmail.com>
2025-03-19 16:13:58 +0100

    init_cv

---
+++ src/main.js
@@ -0,0 +1,163 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+   <h1>Jesus Roldan Perez de Siles</h1>
+   <figure>
+   <img src="/public/zen05.jpg">
+   <figcaption>Motivation, rigueur, persévérance, écoute et très bon esprit d'équipe sont mes principales qualités.</figcaption>
+   </figure>
+  <h2>Expériences professionnelles</h2>
+  <table>
+    <tbody>
+      <tr>
+        <td>Mai 2018 à actuel</td>
+        <td> Omnex </td>
+      </tr>
+      <tr>
+        <td>Responsable Maintenances et Production</td>
+      </tr>
+      <tr>
+        <td>     Principal tâches :        </td>
+        <td> 
+          <ul>
+            <li>Maintenances</li>
+            <li>Gestion du stock</li>
+            <li>Réception des marchandises </li>
+            <li>Contrôle de qualité</li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>Août 2013 à Déc. 2013 et Sept. 2014 à Déc. 2014</td>
+        <td>Imprimerie E. Oleffe</td>
+      </tr>
+      <tr>
+        <td>Magasinier</td>
+      </tr>
+      <tr>
+        <td>     Principal tâches :        </td>
+        <td>   
+          <ul>
+            <li>Gestion du stock</li>
+            <li>Réception des marchandises</li>
+            <li>Préparation des commandes </li>
+            <li>Approvisionnement des zones de production</li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>Août. 2010 à Oct. 2012 </td>
+        <td>A.C. Montessori Kids</td>
+      </tr>
+      <tr>
+        <td>Ouvrier polyvalent</td>
+      </tr>
+      <tr>
+        <td>Principal tâches :</td>
+        <td> 
+          <ul>
+            <li>Chauffeur de bus matin et soir</li>
+            <li>Entretien des jardins et maintenance des locaux de l'école</li>
+            <li>Réparations diverses du mobilier</li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>Mai. 2010 – Oct. 2010 </td>
+        <td>Boum Balle Bulle SPRL</td>
+      </tr>
+      <tr>
+        <td>Ouvrier polyvalent</td>
+      </tr>
+      <tr>
+        <td>Principal tâches :</td>
+         <td> 
+          <ul>
+            <li>Accueil</li>
+            <li>Maintenance et entretien du bâtiment </li>
+            <li>Surveillance piscine</li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>Nov. 2008 – Sept. 2009 </td>
+        <td>C.E.F. Comptoir Electrique Français, Espagne</td>
+      </tr>
+      <tr>
+        <td>Employé administratif</td>
+      </tr>
+      <tr>
+        <td>     Principal tâches :        </td>
+        <td> 
+          <ul>
+            <li>Service clientèle</li>
+            <li>Gestion des documents administratifs et de la facturation de 7 grands magasins établis en Espagne </li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>Avr. 2007 – Fév. 2008 </td>
+        <td>Grupo A.E.A, Espagne</td>
+      </tr>
+      <tr>
+        <td>Comptable</td>
+      </tr>
+      <tr>
+        <td>Principal tâches :</td>
+        <td> 
+          <ul>
+            <li>Gestion comptable de quartiers résidentiels</li>
+          </ul>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <h2>formation</h2>
+  <table>
+    <tbody>
+      <tr>
+        <td>Sep.2024 à actuel</td>
+        <td>IFOSUP - Formation professionnelle Développeur Web</td>
+      </tr>
+      <tr>
+        <td>Oct. 2017 à Mai 2018</td>
+        <td>FOREM - Formation professionnelle Mécanique et Hydraulique </td>
+      </tr>
+      <tr>
+        <td>Juil. 2015 à juin 2016</td>
+        <td>FOREM - Formation professionnelle Automatismes et Pneumatique</td>
+      </tr>
+      <tr>
+        <td>Sept. 2013 à Juin 2015</td>
+        <td>IFOSUP - Formation Supérieure Électricien industriel</td>
+      </tr>
+      <tr>
+        <td>Fév. 2010 à Avr. 2010</td>
+        <td>CLL Centre de langues - Certificat niveau B2</td>
+      </tr>
+      <tr>
+        <td>Sept. 2003 à Juil. 2006</td>
+        <td>I.E.S. Río Verde - Études supérieures d’administration et de gestion</td>
+      </tr>
+    </tbody>
+  </table>
+  <h2>langues</h2>
+  <ul>
+    <li>Espagnol :	Langue maternelle</li>
+    <li>Français : 	Bonnes connaissances</li>
+    <li>Anglais : 	Connaissance de base</li>
+  </ul>
+  <h2>informatique</h2>
+  <ul>
+    <li>	MS-Office (Word, Excel, Outlook, Internet).</li>
+    <li>	Programmation en langage PL7 Pro pour A.P.I. TELEMECANIQUE MICRO et PREMIUM.</li>
+    <li>	Programmation en langage S7 pour les A.P.I. SIEMENS S7 300/400.</li>
+    <li></li>  
+  </ul>
+
+  </div>
+`
+
+
